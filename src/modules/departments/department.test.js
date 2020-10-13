b0c8fe91092c2d1d48b97226fc2ed302825adaa4
Karl Agunday <kaagunday@gmail.com>
2020-10-13 19:41:47 +0800

    refactor tests for department

--- src/modules/departments/department.test.js
+++ src/modules/departments/department.test.js
@@ -1,77 +1,76 @@
 const expect = require('expect')
+const faker = require('faker')
 const { request } = require('../../utils/test')
 const { AuthenticationError } = require('apollo-server-express')
 const { generateToken } = require('../../utils/generate-token')
-const faker = require('faker')
 
-// retrieve without a token
-describe('department', () => {
-  describe('create', () => {
-    it('should not retrieve departments', () => {
-      return request({
-        query: `
-          query {
-            departments {
-              id
-            }
-          }
-        `
-      })
-        .expect(res => {
-          expect(res.body).toHaveProperty('errors')
-        })
-        .expect(200)
-    })
-  })
-})
-
-const testDepartment = {
-  name: faker.commerce.department(),
-  code: faker.random.alphaNumeric(4),
-  description: faker.lorem.sentence(),
-  slug: faker.commerce.slug,
-  pricing: faker.random.float(),
-  seoTitle: faker.lorem.sentence(5),
-  seoKeywords: faker.lorem.words(5),
-  seoDescription: faker.lorem.paragraph(5)
-}
-
-// create with a valid token
 generateToken()
   .then(response => {
-    let token = response
-    const createDepartment = ({
-      name,
-      code,
-      description,
-      slug,
-      pricing,
-      seoTitle,
-      seoKeywords,
-      seoDescription
-    }, returnValues = `{
-      id
-      name
-    }`) => {
-      return request({
-        query: `
-          mutation {
-            createDepartment(
-              name: "${name}",
-              code: "${code}",
-              description: "${description}",
-              slug: "${slug}",
-              pricing: ${pricing},
-              seoTitle: "${seoTitle}",
-              seoKeywords: "${seoKeywords}",
-              seoDescription: "${seoDescription}"
-            ) ${returnValues}
-          }
-        `
-      }).set('x-token', token)
-    }
     describe('department', () => {
-      describe('create', () => {
+      let token = response
+
+      // retrieve without a token
+      describe('retrieve without token', () => {
+        it('should not retrieve departments', () => {
+          return request({
+            query: `
+            query {
+              departments {
+                id
+              }
+            }
+            `
+          })
+            .expect(res => {
+              expect(res.body).toHaveProperty('errors')
+            })
+            .expect(200)
+        })
+      })
+
+      // create with a valid token
+      const testDepartment = {
+        name: faker.commerce.department(),
+        code: faker.random.alphaNumeric(4),
+        description: faker.lorem.sentence(),
+        slug: faker.commerce.slug,
+        pricing: faker.random.float(),
+        seoTitle: faker.lorem.sentence(5),
+        seoKeywords: faker.lorem.words(5),
+        seoDescription: faker.lorem.paragraph(5)
+      }
+      const createDepartment = ({
+        name,
+        code,
+        description,
+        slug,
+        pricing,
+        seoTitle,
+        seoKeywords,
+        seoDescription
+      }, returnValues = `{
+        id
+        name
+      }`) => {
+        return request({
+          query: `
+            mutation {
+              createDepartment(
+                name: "${name}",
+                code: "${code}",
+                description: "${description}",
+                slug: "${slug}",
+                pricing: ${pricing},
+                seoTitle: "${seoTitle}",
+                seoKeywords: "${seoKeywords}",
+                seoDescription: "${seoDescription}"
+              ) ${returnValues}
+            }
+          `
+        }).set('x-token', token)
+      }
+
+      describe('create with a valid token', () => {
         it('should create a new department', () => {
           return createDepartment(testDepartment)
             .expect(res => {
@@ -81,10 +80,8 @@ generateToken()
             .expect(200)
         })
       })
-    })
 
-    describe('department', () => {
-      describe('create', () => {
+      describe('create with non-unique code', () => {
         it('should not create a deparment if code is not unique', () => {
           // attempt to create a department of the same data
           return createDepartment(testDepartment)
@@ -94,21 +91,20 @@ generateToken()
             })
         })
       })
-    })
 
-    const retrieveDepartments = () => {
-      return request({
-        query: `
-          query {
-            departments {
-              id
-              name
+      const retrieveDepartments = () => {
+        return request({
+          query: `
+            query {
+              departments {
+                id
+                name
+              }
             }
-          }
-        `
-      }).set('x-token', token)
-    }
-    describe('department', () => {
+          `
+        }).set('x-token', token)
+      }
+
       describe('retrieve all', () => {
         it('should retrieve all departments', () => {
           return retrieveDepartments()
