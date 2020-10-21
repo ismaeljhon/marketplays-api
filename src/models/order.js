4fc2e27e1d89756c7c022f13053a7f1bce9c7a45
Karl Agunday <kaagunday@gmail.com>
2020-10-21 19:59:09 +0800

    define order model

---
+++ src/models/order.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const orderSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  orderNumber: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  changed: {
+    type: Date,
+    default: Date.now
+  },
+  items: {
+    type: [String] // @TODO - this will be an array of products or services
+  }
+})
+
+const Order = mongoose.model('Order', orderSchema)
+
+module.exports = Order
