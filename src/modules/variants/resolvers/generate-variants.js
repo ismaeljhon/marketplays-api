791d0c9f48a223491ff709cb7d434c2c5416ef5d
Karl Agunday <kaagunday@gmail.com>
2020-11-23 23:09:52 +0800

    allow generation of variants out of attributes and options

---
+++ src/modules/variants/resolvers/generate-variants.js
@@ -0,0 +1,24 @@
+const { schemaComposer } = require('graphql-compose')
+const Variant = require('../../../models/variant')
+const VariantTC = schemaComposer.getOTC('User')
+
+// endpoint for generating variants
+VariantTC.addResolver({
+  name: 'generateVariants',
+  type: 'GenerateVariantsPayload',
+  args: {
+    record: 'GenerateVariantsInput'
+  },
+  description: 'Generates all the possible variants out of the attributes and options',
+  resolve: async ({ source, args }) => {
+    return {
+      record: {
+        variants: await Variant.generateMany(args.record)
+      }
+    }
+  }
+})
+
+schemaComposer.Mutation.addFields({
+  generateVariants: VariantTC.getResolver('generateVariants')
+})
