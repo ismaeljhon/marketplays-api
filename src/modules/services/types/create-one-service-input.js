47977dd5db66363e53e56d1a9f0ea766b005c401
Karl Agunday <kaagunday@gmail.com>
2020-12-30 14:20:41 +0800

    reinstate default input types back. @TODO - default types should be in a separate file and should be loaded first

--- src/modules/services/types/create-one-service-input.js
+++ src/modules/services/types/create-one-service-input.js
@@ -1,6 +1,34 @@
 const { schemaComposer } = require('graphql-compose')
 const ServiceTC = schemaComposer.getOTC('Service')
 
+// modify service input.attribute
+// to instead accept an array of attribute data
+schemaComposer.createInputTC({
+  name: 'AttributeInput',
+  fields: {
+    name: 'String',
+    options: '[String]'
+  }
+})
+
+// modify service input.variants
+// to instead accept an array of variant data
+schemaComposer.getITC('VariantAttributeDataInput')
+  .setFields({
+    attribute: 'String!',
+    option: 'String!'
+  })
+
+schemaComposer.createInputTC({
+  name: 'VariantInput',
+  fields: {
+    name: 'String!',
+    description: 'String',
+    pricing: 'Float!',
+    attributeData: '[VariantAttributeDataInput]'
+  }
+})
+
 schemaComposer.getITC('CreateOneServiceInput')
   .removeField(['attributes', 'variants'])
   .addFields({
