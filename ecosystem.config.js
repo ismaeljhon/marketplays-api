94eb823018c42cb98065b2a14f2720fbfddee6ae
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2021-01-03 19:26:22 +0800

    add node_env again

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,13 @@ module.exports = {
     {
       name: 'BackendStaging',
       script: 'npm run staging',
-      watch: '.'
+      watch: '.',
+      env: {
+        'NODE_ENV': 'development'
+      },
+      env_production: {
+        'NODE_ENV': 'production'
+      }
     }
   ],
 
