8bc01640d6f608b5c4c53665a261fa66523a7958
Karl Agunday <kaagunday@gmail.com>
2021-01-08 16:59:56 +0800

    update model autoloading to avoid recursion

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,6 @@
 const glob = require('glob')
 const path = require('path')
-const getModel = require('../utils/get-model')
+const mongoose = require('mongoose')
 
 // load each file in the models directory
 let models = []
@@ -8,7 +8,16 @@ glob.sync('./src/models/!(index).js').forEach(file => {
   // only load models that havent been loaded yet
   const filename = path.basename(file, path.extname(file))
   const modelName = filename.charAt(0).toUpperCase() + filename.slice(1)
-  models.push(getModel(modelName))
+  let model = null
+  try {
+    model = mongoose.model(modelName)
+  } catch (error) {
+    // @TODO - throw? not necessary cos im expecting `try` to fail if model doesn't exist
+  }
+  if (!model) {
+    model = require(path.resolve(file))
+  }
+  models.push(model)
 })
 
 module.exports = models
