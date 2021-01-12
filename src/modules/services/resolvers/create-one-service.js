2012d24a2dcbbccbd678fc19a2d8e24dbd223820
Karl Agunday <kaagunday@gmail.com>
2021-01-12 14:33:05 +0800

    rewrite custom createOneService route to use

--- src/modules/services/resolvers/create-one-service.js
+++ src/modules/services/resolvers/create-one-service.js
@@ -1,29 +1,18 @@
 const { schemaComposer } = require('graphql-compose')
 const Service = require('../../../models/service')
 
-// reimplement creation of a service
 const ServiceTC = schemaComposer.getOTC('Service')
-const oldResolver = ServiceTC.getResolver('createOne')
-ServiceTC.addResolver({
-  ...oldResolver,
-  name: 'createNew',
-  resolve: async ({ source, args, context, info }) => {
-    // prepare args
-    // @TODO - calling this via Service model
-    // but the (static) method is defined at (base) Item model
-    // making this call via Item model directly doesn't work for some reason
-    return Service.prepareArgs(args)
-      .then(args => {
-        return ServiceTC.getResolver('createOne')
-          .resolve({ source, args, context, info })
-      })
-      .catch(error => {
-        throw error
-      })
-  }
-})
-
-// apply changes made to the resolver
 schemaComposer.Mutation.addFields({
-  createOneService: ServiceTC.getResolver('createNew')
+  // prepare args before item will be created
+  // @TODO - calling this via Service model
+  // but the (static) method is defined at (base) Item model
+  // making this call via Item model directly doesn't work for some reason
+  createOneService: ServiceTC.getResolver('createOne')
+    .wrapResolve(next => async (rp) => {
+      return Service.prepareArgs(rp.args)
+        .then(args => {
+          rp.args = args
+          return next(rp)
+        })
+    })
 })
