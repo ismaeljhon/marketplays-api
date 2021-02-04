978a12213a1c5aa99cd82a50d36b46e1f61ed7fb
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2021-02-04 23:31:03 +0800

    Update: sign up test

--- src/tests/users/signup.test.js
+++ src/tests/users/signup.test.js
@@ -5,7 +5,6 @@ const { UserFactory } = require('../../utils/factories/')
 describe('SignupUser', () => {
   const fakeUser = UserFactory.generate()
 
-  let user = null
   it('should create a new user', () => {
     return request({
       query: `
@@ -18,7 +17,6 @@ describe('SignupUser', () => {
             record {
               fullName
               email
-              verificationCode
             }
           }
         }
@@ -53,70 +51,5 @@ describe('SignupUser', () => {
         expect(Array.isArray(res.body.errors)).toBe(true)
       })
       .expect(200)
-  })
-
-  it('should verify a registered user by providing its verification code', () => {
-    return request({
-      query: `
-        mutation {
-          verifyUser(record: {
-            code: "${user.verificationCode}"
-          }) {
-            record {
-              email
-              emailVerified
-            }
-          }
-        }
-      `
-    })
-      .expect(res => {
-        expect(res.body).toHaveProperty('data.verifyUser.record')
-
-        // check if the user is verified
-        expect(res.body.data.verifyUser.record.emailVerified).toStrictEqual(true)
-      })
-  })
-
-  it('should reject verification attempt if resulting user is already verified', () => {
-    return request({
-      query: `
-        mutation {
-          verifyUser(record: {
-            code: "${user.verificationCode}"
-          }) {
-            record {
-              email
-              emailVerified
-            }
-          }
-        }
-      `
-    })
-      .expect(res => {
-        expect(res.body).toHaveProperty('errors')
-        expect(Array.isArray(res.body.errors)).toBe(true)
-      })
-  })
-
-  it('should reject verification attempt if no user matches with the code', () => {
-    return request({
-      query: `
-        mutation {
-          verifyUser(record: {
-            code: "somerandomnonexistentcode123"
-          }) {
-            record {
-              email
-              emailVerified
-            }
-          }
-        }
-      `
-    })
-      .expect(res => {
-        expect(res.body).toHaveProperty('errors')
-        expect(Array.isArray(res.body.errors)).toBe(true)
-      })
   })
 })
